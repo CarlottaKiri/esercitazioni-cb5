6dc849b31d0458e7b4b7efcddeb2dfc1383d1319
CarlottaKiri <carlottapt@gmail.com>
2022-11-16 17:00:53 +0100

    ADD: js logic

--- 16-11-22/js/main.js
+++ 16-11-22/js/main.js
@@ -1,2 +1,30 @@
+import { GET, POST } from "./api.js";
+import { q, createCard, uuidv4 } from "./utils.js";
 
+const ul = q(".card_ul");
+const form = document.forms.pokemon;
+const elements = form.elements;
 
+const url = "http://localhost:3000/pokemon";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = {
+    id: uuidv4(),
+    name: elements.pkm_name.value,
+    type: elements.pkm_type.value,
+  };
+
+  POST(url, data).then(() => location.reload());
+});
+
+window.onload = GET(url).then((res) =>
+  res.map((pkm) => {
+    try {
+      createCard(url, ul, pkm?.name, pkm?.type, pkm?.id);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+);
--- 16-11-22/js/utils.js
+++ 16-11-22/js/utils.js
@@ -1,15 +1,42 @@
+import { DELETE } from "./api.js";
+
+const c = (el) => document.createElement(el);
+const q = (el) => document.querySelector(el);
+
 // API
 /**
  * Create an unique hash code
  * @returns string
  */
 function uuidv4() {
-	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-		(
-			c ^
-			(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-		).toString(16)
-	);
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
 }
 
-export { c, q, GET, POST, uuidv4 }
+const createCard = (url, parent, name, type, id) => {
+  const wrapperEl = c("li");
+  const cardEl = c("div");
+  const nameEl = c("p");
+  const typeEl = c("p");
+  const btn = c("button");
+
+  wrapperEl.className = "card_list";
+
+  nameEl.textContent = name.toUpperCase();
+  typeEl.textContent = type;
+  btn.textContent = "Delete";
+
+  btn.addEventListener("click", () => {
+    DELETE(url, id).then(() => location.reload());
+  });
+
+  cardEl.append(nameEl, typeEl, btn);
+  wrapperEl.append(cardEl);
+  parent.appendChild(wrapperEl);
+};
+
+export { c, q, uuidv4, createCard };
