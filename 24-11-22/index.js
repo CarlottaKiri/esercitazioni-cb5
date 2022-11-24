e387cb5959da6d1ea6227533982166c2e4e3f0dc
CarlottaKiri <carlottapt@gmail.com>
2022-11-24 17:06:15 +0100

    24-11-22 fixed

--- 24-11-22/index.js
+++ 24-11-22/index.js
@@ -17,8 +17,7 @@ function moltiplicazione(num1, num2) {
 
 const express = require("express");
 const app = express();
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public"))); //2
+app.use(express.static("public")); //2
 
 app.listen(3000, () => {
   //avvia il server sulla porta 3000
