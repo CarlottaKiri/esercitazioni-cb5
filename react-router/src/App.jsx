ccfc1315ebe664aace1caaf1a56dba5176bc5f43
CarlottaKiri <carlottapt@gmail.com>
2023-01-30 17:50:30 +0100

    30-01-23

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,11 @@
 import styles from "./App.module.scss";
 import { Link } from "react-router-dom";
+import image from "./logo.png";
 
 function App() {
   return (
     <div className={styles.App}>
+      <img className={styles.img} src={image} />
       <h1>Sorry!</h1>
       <h3>This page is currently under construction!</h3>
       <h4>Take a look at our </h4>
